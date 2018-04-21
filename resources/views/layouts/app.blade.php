b3a68894205753865d90ac80cbb42b5d1e564389
whossun <whossun@gmail.com>
2018-04-21 07:33:27 +0000

    Topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
